1c46128900abe6628919ade8e092f9c68ae04c25
Andika Alakate <andikaalakate@gmail.com>
2023-11-30 16:01:35 +0700

    Koreksi

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -13,58 +13,63 @@
 
 <body>
     <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-dark sticky-top shadow bg-color">
-        <div class="container">
-
-            <a class="navbar-brand" href="#" data-tilt> <img src="images/logo.png"
-                    class="d-inline-block align-text-middle me-2" style="width: 40px;"><span class="text-color">VantaBlack Translation - Novel Indo</span></a>
-
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
-                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary shadow">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#" data-tilt>
+                <img src="./pic/logo-web/Black Pink Bold Elegant Monogram Personal Brand Logo.png"
+                    class="d-inline-block align-text-middle me-2" style="width: 40px;">
+                <span class="text-black">VantaTranslation</span>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav ms-auto">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" href="./index.php">Beranda</a>
+                        <a class="nav-link active" aria-current="page" href="#">Beranda</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" href="#">Novel</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="#">Kategori</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="#">Penulis</a>
+                        <a class="nav-link" href="#">Novel</a>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle active" href="#" role="button" data-bs-toggle="dropdown"
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
                             aria-expanded="false">
-                            Profil
+                            Kategori
                         </a>
                         <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="profil-guru.php">Guru</a></li>
-                            <li><a class="dropdown-item" href="#">Siswa</a></li>
+                            <li><a class="dropdown-item" href="#">Romance</a></li>
+                            <!-- Tambahkan kategori lain sesuai kebutuhan -->
                         </ul>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" href="#kontak">Hubungi Kami</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="http://localhost/ws_alakate/admin/" type="button"
-                            class="ms-2 btn btn-outline-warning">Login</a>
+                        <a class="nav-link disabled" aria-disabled="true">Nonaktif</a>
                     </li>
                 </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Cari" aria-label="Cari">
+                    <button class="btn btn-outline-success" type="submit">Cari</button>
+                </form>
             </div>
         </div>
     </nav>
-    <!-- End Navbar -->
+    <!-- Akhir Navbar -->
 
+    <!-- Bagian Konten -->
+    <div class="container mt-4">
+        <!-- Tambahkan konten Anda di sini -->
+        <h1>Selamat Datang di VantaTranslation - Novel Indo</h1>
+        <!-- Tambahkan lebih banyak bagian, daftar novel, atau kategori sesuai kebutuhan -->
+    </div>
+    <!-- Akhir Bagian Konten -->
 
+    <!-- Skrip Bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
         integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
         crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist
+/js/bootstrap.min.js"
         integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
         crossorigin="anonymous"></script>
 </body>
