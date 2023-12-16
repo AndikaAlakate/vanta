37b570d019e9e3e6e07c64fa670211b3fa7569d5
Andika Alakate <andikaalakate@gmail.com>
2023-12-16 10:01:25 +0700

    Update index.php

--- admin/dashboard/index.php
+++ admin/dashboard/index.php
@@ -48,6 +48,18 @@
       <section class="content">
         <div class="container-fluid">
           <div class="row">
+            <div class="col-lg-3 col-6">       
+              <div class="small-box bg-info">
+                <div class="inner">
+                  <h3>150</h3>
+                  <p>Jumlah Buku</p>
+                </div>
+                <div class="icon">
+                  <i class="ion ion-book-outline"></i>
+                </div>
+                <a href="#" class="small-box-footer">Selengkapnya <i class="fas fa-arrow-circle-right"></i></a>
+              </div>
+            </div>
             <div class="col-lg-3 col-6">
               <div class="small-box bg-warning">
                 <div class="inner">
@@ -58,6 +70,18 @@
                   <i class="ion ion-person-add"></i>
                 </div>
                 <a href="admin.php" class="small-box-footer">Selengkapnya <i class="fas fa-arrow-circle-right"></i></a>
+              </div>
+            </div>
+            <div class="col-lg-3 col-6">
+              <div class="small-box bg-danger">
+                <div class="inner">
+                  <h3>65</h3>
+                  <p>Pengunjung</p>
+                </div>
+                <div class="icon">
+                  <i class="ion ion-pie-graph"></i>
+                </div>
+                <a href="#" class="small-box-footer">Selengkapnya <i class="fas fa-arrow-circle-right"></i></a>
               </div>
             </div>
           </div>
