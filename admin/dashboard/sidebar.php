f35a12bd082af2396f53c5a840692e535cdb9e56
Andika Alakate <andikaalakate@gmail.com>
2023-12-16 09:12:43 +0700

    Update sidebar.php

--- admin/dashboard/sidebar.php
+++ admin/dashboard/sidebar.php
@@ -56,7 +56,7 @@ while ($row = $result->fetch_assoc()) {
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
                             <a href="./" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
+                                <i class="fas fa-circle nav-icon"></i>
                                 <p>Dashboard</p>
                             </a>
                         </li>
