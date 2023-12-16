7e05d2fe2bbfe00fcd4bce0bf445423e45ea75ad
Andika Alakate <andikaalakate@gmail.com>
2023-12-16 09:02:19 +0700

    Coba

---
+++ admin/dashboard/dist/js/nav-link.js
@@ -0,0 +1,5 @@
+const navLink = document.querySelector(".nav-link");
+
+document.querySelector(".dash").onclick = () => {
+  navLink.classList.toggle("active");
+};
--- admin/dashboard/sidebar.php
+++ admin/dashboard/sidebar.php
@@ -52,6 +52,7 @@
                         <i class="right fas fa-angle-left"></i>
                     </p>
                 </a>
+<<<<<<< Updated upstream
                 <ul class="nav nav-treeview">
                     <li class="nav-item">
                         <a href="./index.php" class="nav-link active">
@@ -86,6 +87,61 @@
                 </ul>
             </li>
         <!-- <li class="nav-item">
+=======
+            </div>
+        </div>
+
+        <!-- SidebarSearch Form -->
+        <div class="form-inline">
+            <div class="input-group" data-widget="sidebar-search">
+                <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+                <div class="input-group-append">
+                    <button class="btn btn-sidebar">
+                        <i class="fas fa-search fa-fw"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                <!-- Add icons to the links using the .nav-icon class with font-awesome or any other icon font library -->
+                <li class="nav-item menu-open">
+                    <a href="#" class="nav-link active">
+                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <p>
+                            VANTA
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="./" class="nav-link dash">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Dashboard</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="contacts.php" class="nav-link">
+                                <i class="nav-icon fas fa-circle"></i>
+                                <p>
+                                    Contacts
+                                </p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="logout.php" class="nav-link">
+                                <i class="nav-icon fas fa-circle"></i>
+                                <p>
+                                    Logout
+                                </p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <!-- <li class="nav-item">
+>>>>>>> Stashed changes
             <a href="pages/widgets.html" class="nav-link">
             <i class="nav-icon fas fa-th"></i>
             <p>
